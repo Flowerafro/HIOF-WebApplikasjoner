f35022a5b7b8ce6cb7abca309eb14630f556da8f
Flowerafro <linehenriksen0@gmail.com>
2025-09-08 16:10:30 +0200

    Js-oppgaver 1-5 komplett

---
+++ 2-L-js/oppgave0/index.js
@@ -0,0 +1,58 @@
+// Oppgave 1
+
+document.getElementById("remove-btn").addEventListener("click", function () {
+    const removeButton = document.getElementById("remove")
+    removeButton.remove();
+})
+
+//document.body.style.fontFamily = "Arial, sans-serif";
+
+// Oppgave 2
+document.getElementById("change-btn").addEventListener("click", function () {
+    const changeText = document.getElementById("change")
+    changeText.innerText = "tekst byttet!"
+})
+
+// Oppgave 3
+
+document.getElementById("input").addEventListener("input", function (event) {
+    const changeInput = document.getElementById("input-text")
+    changeInput.innerText = event.target.value
+})
+
+// Oppgave 4
+const myList = ["item one", "item two", "item three"];
+
+document.getElementById("write-list").addEventListener("click", function () {
+    const writeList = myList.map(list => `<li>${list}</li>`).join("");
+    document.getElementById("ul").innerHTML = writeList;
+})
+
+// Oppgave 5
+
+document.getElementById("create").addEventListener("click", function () {
+    if (document.getElementById("text").value === "h2") {
+        const h2 = document.createElement("h2");
+        h2.innerText = "h2-element";
+        document.getElementById("placeholder").appendChild(h2);
+    } else if (document.getElementById("text").value === "h3") {
+        const h3 = document.createElement("h3");
+        h3.innerText = "h3-element";
+        document.getElementById("placeholder").appendChild(h3);
+    } else if (document.getElementById("text").value === "p") {
+        const p = document.createElement("p");
+        p.innerText = "p-element";
+        document.getElementById("placeholder").appendChild(p);
+    } else if (document.getElementById("text").value === "span") {
+        const span = document.createElement("span");
+        span.innerText = "span-element";
+        document.getElementById("placeholder").appendChild(span);
+    }
+    else {
+        return alert("du må skrive h2 for å lage et h2-element")
+    }
+})
+
+// Oppgave 6
+// Oppgave 7
+// Oppgave 8
