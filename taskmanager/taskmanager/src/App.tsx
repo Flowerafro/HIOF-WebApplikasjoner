17ac5148012ba1761b7f49cb787a81d588a8ae23
Flowerafro <linehenriksen0@gmail.com>
2025-09-16 15:07:36 +0200

    taskfooter
    ny komp

--- taskmanager/taskmanager/src/App.tsx
+++ taskmanager/taskmanager/src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css';
+import TaskFooter from './components/TaskFooter';
 
 /*import TaskCard from './components/TaskCard';*/
 import TaskList from './components/TaskList';
 
-// prop med key-task
-/* const task = {
-  id: '123',
-  title: 'My Title Works',
-  description: 'My description',
-  dueDate: new Date(),
-}; */
 
 // denne listen må være en liste og hvert element må ha signaturen til Task-type
 const tasks = [
@@ -32,7 +26,9 @@ function App() {
     <div>
       <h1>Task manager</h1>
       {/* <TaskCard task={task} /> */}
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks}>
+        <TaskFooter />
+      </TaskList>
     </div>
   );
 }
---
+++ taskmanager/taskmanager/src/components/TaskFooter.tsx
@@ -0,0 +1,7 @@
+export default function TaskFooter({text}: {
+    text?: string
+}) {
+    return (
+         <footer><p>{text ?? 'Task Manager'} &copy; {new Date().getFullYear()}</p></footer>
+    )
+}
